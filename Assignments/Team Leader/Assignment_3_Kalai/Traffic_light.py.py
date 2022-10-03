7bec9d018d9526f3134d808ac2549fc55d64a367
Kalai <112330449+M-Kalaivani@users.noreply.github.com>
2022-10-03 23:36:15 +0530

    Add files via upload

---
+++ Assignments/Team Leader/Assignment_3_Kalai/Traffic_light.py.py
@@ -0,0 +1,30 @@
+#traffic light for raspberry pi simlulating in pycham with GUI
+import turtle
+import time
+
+
+
+
+
+
+while True:
+
+    print("Red light Blinked - Now vehicle Stop behind zebra cross..")
+    print("Blink!!")
+    time.sleep(2)
+    print("Blink!!")
+
+
+    print("Green light on- Now vehicle can go..")
+    print("Blink!!")
+    time.sleep(3)
+    print("Blink!!")
+
+   
+    print("Yellow light Blinked- Now vehicle Ready to go..")
+    print("Blink!!")
+    time.sleep(1)
+    print("Blink!!")
+
+
+wn.mainloop()
